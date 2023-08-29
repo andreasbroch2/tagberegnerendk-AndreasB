4d781d9f026c0fb6d50afaf6c495db13fc6e7a25
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2023-08-29 15:02:40 +0200

    seo

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -7,6 +7,7 @@ export default function Seo(props) {
         <NextSeo
             title={props.title}
             description={props.description}
+            canonical={props.canonical}
         />
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Seo from "./../components/Seo";
 export default function Home() {
     return (
         <>
-        <Seo title="Forside" description="Udregn gratis og hurtigt en pris på dit nye tag. Indtast blot din adresse." />
+        <Seo title="Forside" description="Udregn gratis og hurtigt en pris på dit nye tag. Indtast blot din adresse." canonical="https://tagberegneren.dk/" />
         <LandingPage home={true} />
         </>
     );

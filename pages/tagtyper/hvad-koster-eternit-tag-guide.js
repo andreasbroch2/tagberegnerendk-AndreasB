3a568561fb24960486dab368bb74b64cfc8395df
andreasbroch2 <68319479+andreasbroch2@users.noreply.github.com>
2023-09-05 10:08:56 +0200

    faq

--- pages/tagtyper/hvad-koster-eternit-tag-guide.js
+++ pages/tagtyper/hvad-koster-eternit-tag-guide.js
@@ -4,6 +4,28 @@ import TagTyperLayout from "../../components/ArticleLayout";
 import eternitTag from "../../assets/eternittag.jpg"
 
 export default function Page(props) {
+    const mainEntity = [
+        {
+            questionName: "Hvad er eternit tag?",
+            acceptedAnswerText: "Eternit tag er en type tagbelægning, som består af fibercement, et materiale lavet af cement, sand og fibre. Eternit tag har mange fordele, såsom lav pris, nem montering, lang holdbarhed, god isoleringsevne, brandhæmmende virkning og miljøvenlighed.",
+        },
+        {
+            questionName: "Hvilke typer af eternit tag findes der?",
+            acceptedAnswerText: "Der findes tre hovedtyper af eternit tag: bølgeeternit, eternitskifer og toscana. Bølgeeternit er den mest udbredte type, som har en bølget form og en glat overflade. Eternitskifer er en type, som ligner naturskifer i udseende og har en ru overflade. Toscana er en type, som har et middelhavsinspireret design og en glat overflade.",
+        },
+        {
+            questionName: "Hvad koster et nyt eternit tag pr. m2?",
+            acceptedAnswerText: "Prisen på et nyt eternit tag pr. m2 afhænger af flere faktorer, såsom typen af eternit tag, undertagets tilstand, tagets størrelse og form, monteringsomkostninger og eventuelle ekstra tilbehør som ovenlysvinduer eller skorstene. Som udgangspunkt kan du budgettere med en pris på mellem 650-1200 kroner pr. m2 inklusive montering og moms. Du kan også bruge en prisberegner for at få et mere præcist estimat.",
+        },
+        {
+            questionName: "Hvordan vedligeholder man et eternit tag?",
+            acceptedAnswerText: "Et eternit tag kræver ikke meget vedligeholdelse, men det er stadig vigtigt at tjekke taget regelmæssigt for eventuelle skader eller utætheder. Hvis du opdager revner, huller eller mos på tagpladerne, bør du kontakte en professionel taglægger for at få dem repareret eller udskiftet. Du bør også rense tagrenderne for blade og skidt mindst to gange om året.",
+        },
+        {
+            questionName: "Hvornår skal man skifte et eternit tag?",
+            acceptedAnswerText: "Det kan være svært at sige præcist, hvornår et eternit tag skal skiftes, da det afhænger af tagets tilstand og alder. Generelt kan man sige, at hvis taget er over 40 år gammelt, bør man overveje at få det undersøgt af en fagmand for at vurdere, om det er tid til et nyt tag. Nogle tegn på, at taget er slidt eller beskadiget, er f.eks. utætte tagplader, løse eller manglende lægter, fugt eller skimmel i loftet eller på væggene.",
+        },
+    ]
     return (
         <>
             <Seo
@@ -14,7 +36,7 @@ export default function Page(props) {
             props={props.data}
             />
             <section className="blog-section">
-                <TagTyperLayout props={props.data} image={eternitTag} canonical="https://www.tagberegneren.dk/tagtyper/hvad-koster-eternit-tag-guide">
+                <TagTyperLayout props={props.data} image={eternitTag} canonical="https://www.tagberegneren.dk/tagtyper/hvad-koster-eternit-tag-guide" mainEntity={mainEntity}>
                     {props.data.content}
                 </TagTyperLayout>
             </section>
--- pages/tagtyper/hvad-koster-tagpap-tag-guide.js
+++ pages/tagtyper/hvad-koster-tagpap-tag-guide.js
@@ -4,6 +4,28 @@ import TagTyperLayout from "../../components/ArticleLayout";
 import tagPapTag from "../../assets/tagpap-tag.png"
 
 export default function Page(props) {
+    const mainEntity = [
+        {
+            questionName: "Hvad koster et nyt tagpap tag pr. m2?",
+            acceptedAnswerText: "Prisen på et nyt tagpap tag pr. m2 afhænger af flere faktorer, såsom tagets størrelse, form, kvalitet, montering og antal lag. Hvis du får en professionel til at montere det, kan du forvente en pris på mellem 1.000-1.500 kroner pr. m2. Hvis du selv lægger tagpappet, kan du spare penge og købe det for ca. 400-600 kroner pr. m2",
+        },
+        {
+            questionName: "Hvad er fordelene ved et tagpap tag?",
+            acceptedAnswerText: "Et tagpap tag har mange fordele, såsom lav pris, nem montering, lang holdbarhed (op til 50 år), god isoleringsevne, modstandsdygtighed over for vind og vejr og mulighed for at vælge mellem forskellige farver og typer.",
+        },
+        {
+            questionName: "Hvad er forskellen på 1-lags og 2-lags tagpap?",
+            acceptedAnswerText: "1-lags og 2-lags tagpap er to typer af tagpap, som har forskellige egenskaber og krav til underlaget. 1-lags tagpap består af en enkelt bane af polyesterfibre med bitumen (et bindestof), som brændes fast på en plade eller bræddebeklædning oven på tagspærerne. 2-lags tagpap består af to baner af polyesterfibre med bitumen, som lægges oven på hinanden med en overlapning på ca. 10 cm1. 2-lags tagpap er mere slidstærkt og tæt end 1-lags tagpap, men kræver også et mere stabilt og plant underlag.",
+        },
+        {
+            questionName: "Hvordan vedligeholder man et tagpap tag?",
+            acceptedAnswerText: "Et tagpap tag kræver ikke meget vedligeholdelse, men det er stadig vigtigt at tjekke taget regelmæssigt for eventuelle skader eller utætheder. Hvis du opdager revner, huller eller mos på tagpappet, bør du kontakte en professionel taglægger for at få det repareret eller udskiftet. Du bør også rense tagrenderne for blade og skidt mindst to gange om året.",
+        },
+        {
+            questionName: "Hvornår skal man skifte et tagpap tag?",
+            acceptedAnswerText: "Det kan være svært at sige præcist, hvornår et tagpap tag skal skiftes, da det afhænger af tagets tilstand og alder. Generelt kan man sige, at hvis taget er over 40 år gammelt, bør man overveje at få det undersøgt af en fagmand for at vurdere, om det er tid til et nyt tag. Nogle tegn på, at taget er slidt eller beskadiget, er f.eks. utætte tagsten, løse eller manglende lægter, fugt eller skimmel i loftet eller på væggene.",
+        },
+    ]
     return (
         <>
             <Seo
@@ -14,7 +36,7 @@ export default function Page(props) {
             props={props.data}
             />
             <section className="blog-section">
-                <TagTyperLayout props={props.data} image={tagPapTag} canonical="https://www.tagberegneren.dk/tagtyper/hvad-koster-tagpap-tag-guide">
+                <TagTyperLayout props={props.data} image={tagPapTag} canonical="https://www.tagberegneren.dk/tagtyper/hvad-koster-tagpap-tag-guide" mainEntity={mainEntity}>
                     {props.data.content}
                 </TagTyperLayout>
             </section>
